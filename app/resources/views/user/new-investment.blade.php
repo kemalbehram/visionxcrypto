afe5e4127d8b40bc0b95f2be03849aac124b5473
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-01-07 22:53:47 +0100

    fixing web issue

--- app/resources/views/user/new-investment.blade.php
+++ app/resources/views/user/new-investment.blade.php
@@ -11,10 +11,10 @@
 						<!-- Page Header -->
 						<div class="page-header">
 							<div>
-								<h2 class="main-content-title tx-24 mg-b-5">VX Vault</h2>
+								<h2 class="main-content-title tx-24 mg-b-5">New VX Vault</h2>
 								<ol class="breadcrumb">
 									<li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-									<li class="breadcrumb-item active" aria-current="page">VX Vault</li>
+									<li class="breadcrumb-item active" aria-current="page">New VX Vault</li>
 								</ol>
 							</div>
 						</div>
@@ -22,39 +22,104 @@
 
 						<!-- Row -->
 						<div class="row row-sm">
-							 
-							@foreach($plans as $data)
-															@php
-                        $time_name = \App\TimeSetting::where('time', $data->times)->first();
-                    @endphp
-							<div class="col-xl-3 col-md-6 col-sm-12 col-lg-3">
-								<div class="card custom-card pricingTable2 info">
-									<div class="pricingTable2-header">
-										<h3>{{$data->name}}</h3>
-										<span>{{$data->name}} VX Plan</span>
+							
+							
+							<div class="col-lg-12 col-xl-12 col-md-12">
+							<form id="investnow"  action="{{route('coin-vest')}}" method="post">
+							@csrf
+							<script type="text/javascript">
+
+													function goDoSomething2(identifier){
+
+
+													 document.getElementById("ffinalamount").innerHTML = "$"+$(identifier).val() ;
+													 document.getElementById("btc").innerHTML = {{$btcrate}}*$(identifier).val();
+													 if({{$plan->interest_status}} == 1){
+													 document.getElementById("return").innerHTML = {{$plan->interest}}*$(identifier).val();
+													 }
+													 else{
+													 document.getElementById("return").innerHTML = {{$plan->interest}}*$(identifier).val();
+														 }
+													 
+													 total = {{$btcrate}}*$(identifier).val();
+													 document.getElementById("totalbtc").innerHTML = total.toFixed(8)+"BTC";
+													  }
+													 </script>
+
+								<div class="card custom-card">
+									<div class="card-body">
+										 
+											<div class="form-group mb-0"> <label><b>Enter Amount</b></label>
+												<div class="input-group"> <input type="number" onkeyup="goDoSomething2(this);" name="amount" class="form-control coupon" placeholder="$0.00"> <span class="input-group-append"> <button class="btn btn-primary btn-apply coupon"><i class="fa fa-usd"></i></button> </span> </div>
+											</div>
+											<br>
+										    <div class="form-group mb-0"> <label><b>Select Payment Type</b></label>
+												<div class="input-group"> 
+												<select class="form-control select "  name="wallet_type">
+												@foreach($wallets as $k=>$data)
+												<option value="{{$data->id}}"> {{__(str_replace('_',' ',$data->type))}} ${{number_format($data->balance, 2)}}</option>
+												@endforeach
+												 <option value="1982100101281"> Deposit_Wallet {{__($basic->currency_sym)}}{{number_format(Auth::user()->balance, $basic->decimal)}}</option>
+												  <option value="82718271565131"> Scan QR Code</option>
+											</select>
+
+												<span class="input-group-append"> <button class="btn btn-primary btn-apply coupon"><i class="fa fa-wallet"></i></button> </span> </div>
+											</div>
+										 
 									</div>
-									<div class="pricing-plans">
-										<span class="price-value1">{{$data->interest}} @if($data->interest_status == 1) % @else $ @endif</span></span>
-										<span class="month">/{{$time_name->name}}</span>
+								</div>
+								@php
+									$time_name = \App\TimeSetting::where('time', $plan->times)->first();
+								@endphp
+								<div class="card custom-card cart-details">
+									<div class="card-body">
+										<h5 class="mb-3 font-weight-bold tx-14">INVESTMENT DETAILS</h5>
+										<dl class="dlist-align">
+											<dt class="">Name</dt>
+											<dd class="text-right ml-auto">{{$plan->name}}</dd>
+										</dl>
+										<dl class="dlist-align">
+											<dt>Minimum:</dt>
+											<dd class="text-right text-primary ml-auto">${{number_format($plan->minimum,2)}}</dd>
+										</dl>
+										<dl class="dlist-align">
+											<dt>Maximum:</dt>
+											<dd class="text-right text-primary ml-auto">${{number_format($plan->maximum,2)}}</dd>
+										</dl>
+										<dl class="dlist-align">
+											<dt>Interest:</dt>
+											<dd class="text-right text-success ml-auto">{{$plan->interest}} @if($plan->interest_status == 1) % @else USD @endif</dd>
+										</dl>
+										<dl class="dlist-align">
+											<dt>Payment Cycle:</dt>
+											<dd class="text-right text-success ml-auto">{{$time_name->name}}</dd>
+										</dl>
+										<dl class="dlist-align">
+											<dt>Duration:</dt>
+											<dd class="text-right text-success ml-auto">@if($plan->lifetime_status == 0) {{__($plan->repeat_time)}}{{__($time_name->slug)}} @else @lang('Lifetime') @endif</dd>
+										</dl>
+										<hr>
+										<dl class="dlist-align">
+											<dt>BTC Invested:</dt>
+											<dd class="text-right  ml-auto"><strong id="totalbtc">0.00BTC</strong></dd>
+										</dl>
+										
+										<dl class="dlist-align">
+											<dt>Expected Return:</dt>
+											<dd class="text-right  ml-auto"><strong id="return">0.00USD</strong></dd>
+										</dl>
+										
+					        			<input value="{{$plan->id}}" hidden name="plan_id">
+										<div class="step-footer">
+											<button type="submit" class="step-btn btn btn-primary btn-block">Continue</button>
+										</div> 
 									</div>
-									<div class="pricingContent2">
-										<ul>
-											<li><b>Minimum Investment</b> ${{number_format($data->minimum,2)}}</li>
-											<li><b>Maximum Investment</b> ${{number_format($data->maximum,2)}}</li>
-											<li><b>Capital access</b> after 3 months</li>
-											<li><b>99.9%</b> Secure</li>
-											<li><b>KYC/BVN</b> verification</li>
-											<li><b>24/7</b> Support</li>
-										</ul>
-									</div><!-- CONTENT BOX-->
-									<div class="pricingTable2-sign-up">
-										<a href="{{route('newcoinvest',$data->id)}}" class="btn btn-block btn-primary">get started</a>
-									</div><!-- BUTTON BOX-->
 								</div>
 							</div>
-						@endforeach
+							</form>
 						</div>
 						<!-- End Row -->
+
 					</div>
 				</div>
 			</div>
