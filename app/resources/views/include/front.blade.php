43a3f79d88d1d5591615c8cb2297c50eba88e246
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-01-17 14:14:02 +0100

    push all

--- app/resources/views/include/front.blade.php
+++ app/resources/views/include/front.blade.php
@@ -140,7 +140,7 @@
 
         <!-- Start header -->
         <header class="header-nav-center header-nav-left no_blur crypto_1 light nav-product active-orange"
-                id="myNavbar">
+                id="myNavbar"  style="background-color: #010137">
             <div class="container">
                 <!-- navbar -->
 
@@ -153,7 +153,7 @@
                             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false"
                             aria-label="Toggle navigation">
-                        <svg viewBox="0 0 64 48">
+                        <svg viewBox="0 0 64 48" style="stroke: white">
                             <path d="M19,15 L45,15 C70,15 58,-2 49.0177126,7 L19,37"></path>
                             <path d="M19,24 L45,24 C61.2371586,24 57,49 41,33 L32,24"></path>
                             <path d="M45,33 L19,33 C-8,33 6,-2 22,14 L45,37"></path>
