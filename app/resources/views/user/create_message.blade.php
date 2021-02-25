659c77c79b867584b6747768287d39e09fb5e6fa
khaytechdigitalz <60061496+khaytechdigitalz@users.noreply.github.com>
2021-02-25 02:31:13 +0100

    Update create_message.blade.php

--- app/resources/views/user/create_message.blade.php
+++ app/resources/views/user/create_message.blade.php
@@ -1,25 +1,66 @@
 @extends('include.userdashboard')
 
 @section('content')
+<<!-- Main Content-->
+			<div class="main-content side-content pt-0">
 
-<div class="page-header"><div class="container"><div class="row justify-content-center"><div class="col-lg-8 col-xl-7 text-center"><h2 class="page-title">Create New Message</h2></div></div></div><!-- .container --></div><div class="page-content"><div class="container"><div class="row">
+				<div class="container-fluid">
+					<div class="inner-body">
 
+						<!-- Page Header -->
+						<div class="page-header">
+							<div>
+								<h2 class="main-content-title tx-24 mg-b-5">Create Ticket</h2>
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+									<li class="breadcrumb-item active" aria-current="page">Tickets</li>
+								</ol>
+							</div>
+						</div>
+						<!-- End Page Header -->
 
-<div class="col-lg-12">
+						 
 
+						<!-- Row -->
+						<div class="row row-sm">
+							<div class="col-lg-12 col-md-12">
+								<div class="card custom-card main-content-body-profile">
+									<div class="tab-content">
+
+										<div class="main-content-body tab-pane p-4 border-top-0 active" id="edit">
+											<div class="card-body border">
+												<div class="mb-4 main-content-label">Create Support Ticket</div>
 <form action="{{route('post.message')}}" method="post" enctype="multipart/form-data">
 @csrf
-<div class="content-area card"><div class="card-innr card-innr-fix"><div class="card-head"><h6 class="card-title">Send Us A Message</h6></div><div class="gaps-1x"></div><!-- .gaps --><form action="#"><div class="row"> <div class="col-md-12"><div class="input-item input-with-label"><label class="input-item-label text-exlight">Message Code</label><input value="{{$code}}" name="code" class="input-bordered" readonly type="text"><label class="input-item-label text-exlight"><small> (Please keep this as you may need it to query your message)</small></label></div></div></div>
+ <div class="row"> <div class="col-md-12"><div class="input-item input-with-label"><label class="input-item-label text-exlight">Ticket Code</label><input value="{{$code}}" name="code" class="form-control" readonly type="text"><label class="input-item-label text-exlight"><small> (Please keep this as you may need it to query your message)</small></label></div></div></div>
 
 
-<div class="input-item input-with-label"><label class="input-item-label text-exlight">Attachment Upload</label><div class="relative"><em class="input-file-icon fas fa-upload"></em><input type="file" name="image" class="input-file" id="file-01"><label for="file-01">Choose a file</label>
+<div class="input-item input-with-label"><label class="input-item-label text-exlight">Attachment Upload</label><div class="relative"><input type="file" name="image" class="form-control" id="file-01"><label for="file-01">Choose a file</label>
 </div>
 <small> (Please attach file to your message if there is any or leave empty and proceed)</small>
 </div>
 
 
 
-<div class="input-item input-with-label"><label class="input-item-label text-exlight">Message Subject</label><input class="input-bordered" name="subject" type="text"></div><div class="input-item input-with-label"><label class="input-item-label text-exlight">Your Message</label><textarea name="body" class="input-bordered input-textarea"></textarea></div><div class="gaps-1x"></div><button class="btn btn-primary">Send Message</button></form></div><!-- .card-innr --></div><!-- .card --></div> <!-- .col --></div><!-- .row --></div><!-- .container --></div>
+<div class="input-item input-with-label"><label class="input-item-label text-exlight">Message Subject</label><input required class="form-control" name="subject" type="text"></div><div class="input-item input-with-label"><label class="input-item-label text-exlight">Your Message</label><textarea name="body" class="form-control input-textarea" required ></textarea></div><div class="gaps-1x"></div><br><button class="btn btn-primary">Send Message</button></form>
+											</div>
+										</div>
+
+									 
 
 
+									 		<!-- main-profile-body -->
+												</div>
+											</div>
+										</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- End Row -->
+					</div>
+				</div>
+			</div>
+			<!-- End Main Content-->
 @endsection
