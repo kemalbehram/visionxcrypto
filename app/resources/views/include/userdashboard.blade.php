a281ffd5388ba9b6e30ac2a5852b30562b97e2ba
khaytechdigitalz <60061496+khaytechdigitalz@users.noreply.github.com>
2021-02-25 09:19:19 +0100

    Update userdashboard.blade.php

--- app/resources/views/include/userdashboard.blade.php
+++ app/resources/views/include/userdashboard.blade.php
@@ -2145,4 +2145,17 @@
 
 	</body>
 </html>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60375c03385de4075719d8c4/1evc3usun';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 @endif
