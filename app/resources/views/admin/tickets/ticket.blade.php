d276a9a14980a9d1e95f43a25d6fc3e8badc8eb6
khaytechdigitalz <60061496+khaytechdigitalz@users.noreply.github.com>
2021-03-01 10:05:34 +0100

    Update ticket.blade.php

--- app/resources/views/admin/tickets/ticket.blade.php
+++ app/resources/views/admin/tickets/ticket.blade.php
@@ -2,10 +2,19 @@
 
 @section('body')
 
-<div class="page-content"><div class="container"><div class="content-area card"><div class="card-innr"><div class="card-head"><h4 class="card-title">User's Message</h4></div><table class="data-table dt-init kyc-list"><thead><tr class="data-item data-head"><th class="data-col dt-user">Sender</th><th class="data-col dt-user">Subject</th> <th class="data-col dt-status">Status</th><th class="data-col"></th></tr></thead><tbody>
+<div class="page-content"><div class="container"><div class="content-area card"><div class="card-innr"><div class="card-head"><h4 class="card-title">User's Message</h4></div><table class="data-table dt-init kyc-list"><thead><tr class="data-item data-head"><th class="data-col dt-user">Sender</th><th class="data-col dt-user">Subject</th> 
+
+<th class="data-col dt-user">Ticket ID</th> 
+
+
+<th class="data-col dt-status">Status</th><th class="data-col"></th></tr></thead><tbody>
 
  @foreach($inbox as $k=>$data)
-<tr class="data-item"><td class="data-col dt-user"><span class="lead user-name">{{$data->user->username}}</span><span class="sub user-id">{!! date(' d/M/Y', strtotime($data->created_at)) !!}</span></td><td class="data-col dt-user"><span class="sub sub-s2 sub-dtype">{{$data->title}}</span></td> <td class="data-col dt-status">
+<tr class="data-item"><td class="data-col dt-user"><span class="lead user-name">{{$data->user->username}}</span><span class="sub user-id">{!! date(' d/M/Y', strtotime($data->created_at)) !!}</span></td><td class="data-col dt-user"><span class="sub sub-s2 sub-dtype">{{$data->title}}</span></td>
+
+<td class="data-col dt-user"><span class="sub sub-s2 sub-dtype">{{$data->code}}</span></td>
+
+<td class="data-col dt-status">
 @if($data->view == 0)
 <span class="dt-status-md badge badge-outline badge-primary badge-md">New</span>
 <span class="dt-status-sm badge badge-sq badge-outline badge-primary badge-md">N</span>
