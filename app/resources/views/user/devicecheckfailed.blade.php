a07926d3faae85be6a36cd9e4d8a50fb2af29e3f
khaytechdigitalz <60061496+khaytechdigitalz@users.noreply.github.com>
2021-03-05 18:24:06 +0100

    Update devicecheckfailed.blade.php

--- app/resources/views/user/devicecheckfailed.blade.php
+++ app/resources/views/user/devicecheckfailed.blade.php
@@ -86,7 +86,8 @@
             <h3 class="main-title">Unauthorized Device Detected</h3>
             <img src="{{asset('assets/images/mobile-banking.png')}}" width="300" alt="" class="icon">
             <div class="confirmation-text font-fix">A text message with a verification code was sent to <span
-                    class="mark-text">{{Auth::user()->phone}}</span>. Please enter the code in the field below to verify
+                    class="mark-text">{{Auth::user()->phone}}</span> and your email address  <span
+                    class="mark-text">{{Auth::user()->email}}</span>.  Please enter the code in the field below to verify
                 your identity
             </div>
             <form class="verification-code-form" action="{{ route('user.vunauthorize')}}" method="post">
