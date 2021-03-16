2188ac43796a5a03fd0ea78d9311e2c5441acf87
khaytechdigitalz <60061496+khaytechdigitalz@users.noreply.github.com>
2021-03-16 18:39:47 +0100

    Update edit.blade.php

--- app/resources/views/admin/currency/edit.blade.php
+++ app/resources/views/admin/currency/edit.blade.php
@@ -16,13 +16,15 @@
 
                                                    </div></div><div class="col-xl-6 col-sm-6"><div class="input-item input-with-label"><label class="input-item-label ucap text-exlight">Currency Symbol</label> <input type="text" class="input-bordered"
                                                placeholder="Currency Symbol" value="{{$currency->symbol}}"
-                                               name="symbol"></div></div><div class="col-xl-6 col-sm-6"><div class="input-item input-with-label"><label class="input-item-label ucap text-exlight">Currency Price</label><input type="text" name="price" value="{{$currency->price}}"
+                                               name="symbol"></div></div>
+                                               
+                                               <!--<div class="col-xl-6 col-sm-6"><div class="input-item input-with-label"><label class="input-item-label ucap text-exlight">Currency Price</label><input type="text" name="price" value="{{$currency->price}}"
                                                   class="input-bordered"
                                                    onkeyup="this.value = this.value.replace (/^\.|[^\d\.]/g, '')"></div></div><div class="col-xl-6 col-sm-6"><div class="input-item input-with-label"><label class="input-item-label ucap text-exlight">Wallet Address</label> <input type="text" name="payment_id" value="{{$currency->payment_id}}"
                                                name="payment_id" class="input-bordered"
                                                placeholder="Payment Id"></div></div></div><div class="row"><div class="col-md-6"><div class="input-item input-with-label"><label class="input-item-label ucap text-exlight">Sell Rate</label> <input type="text" name="sell" value="{{$currency->sell}}" class="input-bordered"
                                                    placeholder="0.00"
-                                                   ></div></div><div class="col-md-6"><div class="input-item input-with-label"><label class="input-item-label ucap text-exlight">Purchase Rate</label> <input type="text" name="buy" value="{{$currency->buy}}"
+                                                   ></div></div> --><div class="col-md-6"><div class="input-item input-with-label"><label class="input-item-label ucap text-exlight">Purchase Rate</label> <input type="text" name="buy" value="{{$currency->buy}}"
                                                    class="input-bordered"
                                                    placeholder="0.00"
                                                    onkeyup="this.value = this.value.replace (/^\.|[^\d\.]/g, '')"></div></div></div> </div><!-- .card-innr -->
